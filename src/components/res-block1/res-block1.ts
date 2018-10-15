b579fc96e43ab3435687a98d3f9d1cb4a2c60b63
xuanphuoc129 <20138686@student.hust.edu.vn@gmail.com>
2018-10-15 23:14:50 +0700

    push code

--- src/components/res-block1/res-block1.ts
+++ src/components/res-block1/res-block1.ts
@@ -1,5 +1,5 @@
 import { Component, ViewChild } from '@angular/core';
-import { Slides } from 'ionic-angular';
+import { Slides, ModalController } from 'ionic-angular';
 
 /**
  * Generated class for the ResBlock1Component component.
@@ -14,9 +14,9 @@ import { Slides } from 'ionic-angular';
 export class ResBlock1Component {
   @ViewChild(Slides) myslide: Slides;
   text: string;
-  url: string = "./assets/imgs/res-block2-b1.jpg";
-  url1: string = "./assets/imgs/res-block2-b2.jpg";
-  url2: string = "./assets/imgs/res-block2-b3.jpg";
+  url: string = "./assets/imgs/res-table1.jpg";
+  url1: string = "./assets/imgs/res-table2.jpg";
+  url2: string = "./assets/imgs/res-1.jpg";
   url3: string = "./assets/imgs/res-block2-b4.jpg";
   url4: string = "./assets/imgs/res-img4.jpg";
   url5: string = "./assets/imgs/res-bl2.1.jpg";
@@ -25,7 +25,10 @@ export class ResBlock1Component {
   url8: string = "./assets/imgs/res-bl2.4.jpg";
   url9: string = "./assets/imgs/res-bl2.5.jpg";
 
-  constructor() {
+  constructor(
+    public modalCtrl: ModalController
+  ) {
+    
     console.log('Hello ResBlock1Component Component');
     this.text = 'Hello World';
   }
@@ -36,5 +39,9 @@ export class ResBlock1Component {
   onBackSlide() {
     this.myslide.slidePrev();
   }
+  // onClickOrder(){
+  //   let pupup = this.modalCtrl.create("OrderPopupPage");
+  //   pupup.present();
+  // }
 
 }
--- src/components/res-block2/res-block2.ts
+++ src/components/res-block2/res-block2.ts
@@ -13,13 +13,22 @@ import { Component } from '@angular/core';
 export class ResBlock2Component {
 
   text: string;
-  url1:string = "./assets/imgs/res-bl2.6.jpg"
-  url2:string = "./assets/imgs/res-bl2.7.jpg"
-  url3:string = "./assets/imgs/res-bl2.8.jpg"
-  url4:string = "./assets/imgs/res-bl2.9.jpg"
+  url1:string = "../assets/imgs/res-bl2.6.jpg";
+  url2:string = "../assets/imgs/res-bl2.7.jpg";
+  url3:string = "../assets/imgs/res-bl2.8.jpg";
+  url4:string = "../assets/imgs/res-bl2.9.jpg";
+  imgs: Array<string> = [
+    "../assets/imgs/res-bl2.6.jpg",
+    "../assets/imgs/res-bl2.7.jpg",
+    "../assets/imgs/res-bl2.8.jpg",
+    "../assets/imgs/res-bl2.9.jpg"
+  ];
   constructor() {
     console.log('Hello ResBlock2Component Component');
     this.text = 'Hello World';
+
   }
+
+
 
 }
